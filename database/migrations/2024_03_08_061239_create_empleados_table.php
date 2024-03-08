4ae1e6a28f13a058ef38fd15f3730c7b05c7098d
ChromeBridge321 <davidgarcia2809@gmail.com>
2024-03-08 05:45:48 -0600

    a
    a

--- database/migrations/2024_03_08_061239_create_empleados_table.php
+++ database/migrations/2024_03_08_061239_create_empleados_table.php
@@ -12,8 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('empleados', function (Blueprint $table) {
-            $table->id();
-            $table->string('Nombre');
+            $table->id('cve_empleados');
             $table->id();
             $table->id();
             $table->id();
